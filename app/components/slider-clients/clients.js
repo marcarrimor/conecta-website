da3595b30d3c5bbdf64faf8dac7a854196c5bd8b
Octavio Martinez Campuzano <octavio.martinez@coredev.io>
2016-06-02 17:31:04 -0500

    Slick

--- app/components/slider-clients/clients.js
+++ app/components/slider-clients/clients.js
@@ -9,6 +9,17 @@
 
     function clientsCtrl(){
         var asController = this;
+
+
+
+        angular.element(document).ready(function () {
+             $(".regular").slick({
+                dots: true,
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 3
+          });
+        });
  
   };
 
