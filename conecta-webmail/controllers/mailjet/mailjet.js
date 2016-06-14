990ec39afb2f5acce1f5cda112d793bf6132eed9
Octavio Martinez Campuzano <octavio.martinez@coredev.io>
2016-06-14 15:03:07 -0500

    new mailjet controller

---
+++ conecta-webmail/controllers/mailjet/mailjet.js
@@ -0,0 +1,88 @@
+var http = require('http')
+  , querystring = require('querystring');
+
+var mail_parser = require('./mail-parser');
+
+// Initialization class
+var Mailjet = function(apiKey, secretKey) {
+  this._apiKey = apiKey;
+  this._secretKey = secretKey;
+  this._authentificate = new Buffer(apiKey + ':' + secretKey).toString('base64');
+};
+
+Mailjet.prototype = {};
+
+// Email sending code
+Mailjet.prototype.sendContent = function(from, to, subject, type, content) {
+
+  if (arguments.length < 4){
+    throw new Error('Missing required argument');
+    return false;}
+
+  if (typeof(to) == 'string')
+      to = [to];
+  var recipients = mail_parser.parse_recipient_type(to);
+  // Build the HTTP POST body text
+
+  if (type == 'html') {
+      var body = querystring.stringify({
+        from: from,
+        // Handle many destinations
+        to: recipients['to'].join(', '),
+        cc: recipients['cc'].join(', '),
+        bcc: recipients['bcc'].join(', '),
+        subject: subject,
+        html: content
+      });
+  }
+  else if (type == 'text') {
+      var body = querystring.stringify({
+        from: from,
+        // Handle many destinations
+        to: recipients['to'].join(', '),
+        cc: recipients['cc'].join(', '),
+        bcc: recipients['bcc'].join(', '),
+        subject: subject,
+        text: content
+      });
+  }
+  else {
+      throw new Error('Wrong email type');
+      return false;
+  }
+
+  var options = {
+    hostname: 'api.mailjet.com',
+    port: 80,
+    path: '/v3/send/',
+    method: 'POST',
+    headers: {
+      'Authorization': 'Basic ' + this._authentificate,
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(body)
+    }
+  };
+
+  // API request
+  var req = http.request(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+    });
+  });
+
+  // Checking errors
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+    return false;
+  });
+
+  // Send the body
+  console.log(body);
+  req.end(body);
+  return true;
+};
+
+module.exports = Mailjet;
--- conecta-webmail/routes/mail.js
+++ conecta-webmail/routes/mail.js
@@ -2,63 +2,43 @@ var express = require('express');
 var router = express.Router();
 var config = require("../keys");
 
-var Mailjet = require('mailjet-sendemail');
+var Mailjet = require('../controllers/mailjet/mailjet.js');
 var mailjet = new Mailjet(config.apiKey, config.secretKey);
 
 
-// mailjet.sendContent('contacto@conecta.co',
-//          ['marisol.carrillo@conecta.co', 'bcc:octavio.martinez@conecta.co'],
-//          'This is a test !',
-//          'html',
-//          '<b>Well, this is working !</b>');
 
-// var mailjet = require ('node-mailjet')
-// 	.connect(config.apiKey, config.secretKey);
-
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('get mail');
 });
 
 router.post('/sendMail', function(req, res){
-	console.log(req);
+	console.log(req.body);
 	var data = {
 				succes: true,
 				mensaje: "Correo enviado satisfactoriamente"
 			}
 	var html = "<html><body><h1>Hola "+req.body.nombre+"</h1></body></html>";
-	// var request = mailjet.post("send");
-
-	var emailData = {
-		"FromEmail":"contacto@conecta.co",
-		"FromName":"Conecta Soluciones Tecnológicas",
-		"Subject":"Contacto",
-		"Html-part": html,
-		"Recipients":[
-				{
-						"Email": req.body.correo
-				}
-		]
-	};
-// request.request(emailData).then(function(result){
-// 	console.log(result);
-// 	res.jsonp(data);
-// }).catch(function(reason){
-// 	console.log(reason);
-// 	data = {
-// 			success: false,
-// 			mensaje: "Ocurrió un error al enviar el correo.",
-// 			reason: reason
-// 		}
-// 	res.jsonp(data);
-// });
 
 
-mailjet.sendContent('contacto@conecta.co',
-         ['marisol.carrillo@conecta.co', 'bcc:octavio.martinez@conecta.co'],
-         'This is a test !',
+
+if(mailjet.sendContent('contacto@conecta.co',
+         [req.body.correo, 'bcc:contacto@conecta.co'],
+         'Contacto Conecta Soluciones Tecnológicas',
          'html',
-         '<b>Well, this is working !</b>');
+         html)){
+                 res.jsonp(data);
+         }else{
+                 	data = {
+                 			success: false,
+                 			mensaje: "Ocurrió un error al enviar el correo."
+                 		}
+                 	res.jsonp(data);
+         }
+
 
 });
+
+
+
+
 module.exports = router;
